5f30a843d0bd842d0e1d1b025a3357086bc77c13
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-02-22 17:54:22 +0530

    ddd

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ test('renders learn react link', () => {
   render(<App />);
   console.log(1)
   const linkElement = screen.getByText(/learn react/i);
-  console.log(12333)
+  console.log(1233)
   expect(linkElement).toBeInTheDocument();
 });
