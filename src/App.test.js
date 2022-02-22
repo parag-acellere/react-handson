594a4676a31e76cc28dec1a0adad6ed2551da740
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-02-22 12:41:48 +0530

    test-6

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  console.log("here here")
   expect(linkElement).toBeInTheDocument();
 });
