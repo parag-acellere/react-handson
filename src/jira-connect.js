8c4bd95e6ba92d8a6d7f810bf01cdca6d455265a
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-05-22 01:22:31 +0530

    cz

--- src/jira-connect.js
+++ src/jira-connect.js
@@ -1,6 +1,7 @@
 function test() {
  test_message = "this is test message";
  console.log(test_message);
+ console.log(12);
  test_lang = "java";
  switch(test_lang) {
 	case "java": console.log("this is java");
