321c8b6130185b97dc8e7008759ff6cfe86f8d3f
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-05-22 01:42:07 +0530

    dasss

--- src/jira-connect.js
+++ src/jira-connect.js
@@ -1,7 +1,7 @@
 function test() {
  test_message = "this is test message";
  console.log(test_message);
- console.log();
+ console.log(test_message);
  test_lang = "java";
  switch(test_lang) {
 	case "java": console.log("this is java");
