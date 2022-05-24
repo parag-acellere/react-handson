93849006fd7f7c2a32e132c0090076fd924e93c3
parag-acellere <parag.dalal@embold.io>
2022-05-24 16:32:37 +0530

    commit test

--- src/App.js
+++ src/App.js
@@ -93,9 +93,6 @@ console.log(profileData);
 console.log(profileData);
 console.log(profileData);
 console.log(profileData);
-console.log(profileData);
-console.log(profileData);
-console.log(profileData);
     // f
   };
   return (
