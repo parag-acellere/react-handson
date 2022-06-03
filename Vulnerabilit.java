835bc6e74cda5801de0ffe0880c6488330021cf4
parag-acellere <parag.dalal@embold.io>
2022-06-03 18:27:37 +0530

    Update Vulnerabilit.java

--- Vulnerabilit.java
+++ Vulnerabilit.java
@@ -2,10 +2,10 @@ Class Vulnerabilit {
 public void foo() {
     // Non-compliant code
         try {            
-            System.out.println("1");
             Cipher c = Cipher.getInstance("DES");
         // Or DESede, RC2, RC4 as these are known to be vulnerable
         } catch (NoSuchAlgorithmException|NoSuchPaddingException e) {
+            System.out.println("1");
             logger.error("Invalid algorithm",e);
         }
     }
