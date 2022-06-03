3da703896596f2c171759711c5957762b88a3d46
parag-acellere <parag.dalal@embold.io>
2022-06-03 17:07:40 +0530

    test-1

---
+++ CipherExample.java
@@ -0,0 +1,16 @@
+
+class CipherExample {
+    public void foo() {
+    // Non-compliant code
+        try {            
+            System.out.println("1");
+            Cipher c = Cipher.getInstance("DES");
+        // Or DESede, RC2, RC4 as these are known to be vulnerable
+        } catch (NoSuchAlgorithmException|NoSuchPaddingException e) {
+            logger.error("Invalid algorithm",e);
+        }
+    }
+}
+
+
+
