6e27f24e366dbebd48c7c31623ed857d28e93684
parag-acellere <parag.dalal@embold.io>
2022-06-06 01:35:08 +0530

    Update CipherExample.java

--- CipherExample.java
+++ CipherExample.java
@@ -3,7 +3,6 @@ class CipherExample {
     public void foo() {
     // Non-compliant code
         try {            
-            System.out.println("1");
             Cipher c = Cipher.getInstance("DES");
         // Or DESede, RC2, RC4 as these are known to be vulnerable
         } catch (NoSuchAlgorithmException|NoSuchPaddingException e) {
