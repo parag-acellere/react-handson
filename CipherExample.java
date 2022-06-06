ccf7e5c6ba00c7bd0adf271af1b3dab54902ef94
parag-acellere <parag.dalal@embold.io>
2022-06-07 02:13:34 +0530

    Update CipherExample.java

--- CipherExample.java
+++ CipherExample.java
@@ -8,7 +8,6 @@ class CipherExample {
         // Or DESede, RC2, RC4 as these are known to be vulnerable
         } catch (NoSuchAlgorithmException|NoSuchPaddingException e) {
              
-            System.out.println("1");
             logger.error("Invalid algorithm",e);
         }
     }
