d3b136e552eff8a3d850af060150c2ed4430e2a1
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-06-14 13:10:39 +0530

    TEST-1

--- src/CipherExample.java
+++ src/CipherExample.java
@@ -4,6 +4,7 @@ class CipherExample {
     // Non-compliant code
         try {            
             Cipher c = Cipher.getInstance("DES");
+		System.out.println("");
         // Or DESede, RC2, RC4 as these are known to be vulnerable
         } catch (NoSuchAlgorithmException|NoSuchPaddingException e) {
             logger.error("Invalid algorithm",e);
