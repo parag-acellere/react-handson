b151ee32b3c5ace417fd2666a341c76b395fbe23
INPUN5CD024HH4L\Parag <parag.dalal@embold.io>
2022-06-15 18:41:24 +0530

    TEST-1

--- src/java/new - Copy.java
+++ src/java/new - Copy.java
@@ -29,7 +29,6 @@ public static boolean writeStreamToFile(InputStream stream, File file) {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-System.out.println("Now the output is redirected!");
 				
 				System.out.println("Now the output is redirected!");
 				out = null;
